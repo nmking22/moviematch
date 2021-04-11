4c850c2bc6e7f37c25a1e02a7976c546f43cd049
Nick King <nickmaxking@gmail.com>
2021-04-11 02:21:15 -0600

    Add movie create route and controller action

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::MoviesController < ApplicationController
   def show
     render json: MovieSerializer.new(Movie.find(params[:id]))
   end
+
+  def create
+    movie = Movie.create(movie_params)
+    render json: MovieSerializer.new(movie)
+  end
+
+  private
+
+    def movie_params
+      params.permit(:title, :tmdb_id)
+    end
 end
