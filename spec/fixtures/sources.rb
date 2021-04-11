f5aa11576eb209f1e00046b7d6e2222cc5c7809a
Nick King <nickmaxking@gmail.com>
2021-04-10 23:42:02 -0600

    Initial commit

---
+++ spec/fixtures/sources.rb
@@ -0,0 +1,69 @@
+# this is a reference file for watchmode source ids
+
+[{
+    "id": 203,
+    "name": "Netflix",
+    "type": "sub",
+    "ios_appstore_url": "http://itunes.apple.com/app/netflix/id363590051",
+    "android_playstore_url": "https://play.google.com/store/apps/details?id=com.netflix.mediaclient&hl=en",
+    "android_scheme": "nflx",
+    "ios_scheme": "nflx",
+    "regions": [
+        "US",
+        "CA",
+        "GB",
+        "AU"
+    ]
+},
+{
+    "id": 157,
+    "name": "Hulu",
+    "type": "sub",
+    "ios_appstore_url": "http://itunes.apple.com/app/hulu-plus/id376510438",
+    "android_playstore_url": "https://play.google.com/store/apps/details?id=com.hulu.plus",
+    "android_scheme": "hulu",
+    "ios_scheme": "hulu",
+    "regions": [
+        "US"
+    ]
+},
+{
+    "id": 26,
+    "name": "Amazon Prime",
+    "type": "sub",
+    "ios_appstore_url": "http://itunes.apple.com/app/amazon-instant-video/id545519333",
+    "android_playstore_url": "http://amazon.com/GetAndroidVideo",
+    "android_scheme": "intent",
+    "ios_scheme": "aiv",
+    "regions": [
+        "US",
+        "GB"
+    ]
+},
+{
+    "id": 387,
+    "name": "HBO MAX",
+    "type": "sub",
+    "ios_appstore_url": null,
+    "android_playstore_url": null,
+    "android_scheme": "hbomax",
+    "ios_scheme": "hbomax",
+    "regions": [
+        "US"
+    ]
+},
+{
+    "id": 372,
+    "name": "Disney+",
+    "type": "sub",
+    "ios_appstore_url": "https://apps.apple.com/app/disney/id1446075923",
+    "android_playstore_url": null,
+    "android_scheme": "disneyplus",
+    "ios_scheme": "disneyplus",
+    "regions": [
+        "US",
+        "CA",
+        "GB",
+        "AU"
+    ]
+}]
