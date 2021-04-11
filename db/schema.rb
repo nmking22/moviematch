fc39658161d43e73ba29cd96bc50740bd8948cf3
Nick King <nickmaxking@gmail.com>
2021-04-11 00:09:29 -0600

    Create and migrate services table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_060132) do
+ActiveRecord::Schema.define(version: 2021_04_11_060858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_04_11_060132) do
     t.float "vote_average"
     t.integer "vote_count"
     t.string "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.integer "watchmode_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
