3fd4ac1ccb05a0e947f60a1fddd0e9966badc14b
Nick King <nickmaxking@gmail.com>
2021-04-11 02:09:26 -0600

    Add route and controller action for movies show

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::MoviesController < ApplicationController
   def index
     render json: MovieSerializer.new(Movie.all)
   end
+
+  def show
+    render json: MovieSerializer.new(Movie.find(params[:id]))
+  end
 end
