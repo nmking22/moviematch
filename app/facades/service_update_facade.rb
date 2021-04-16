9f9fc794764aab114543d12dbff83e4ae1bf18e1
Nick King <nickmaxking@gmail.com>
2021-04-16 01:15:48 -0600

    Move movie and availability creation into active job

--- app/facades/service_update_facade.rb
+++ app/facades/service_update_facade.rb
@@ -3,24 +3,13 @@ class ServiceUpdateFacade
     id = service_watchmode_id(service_name)
     service = Service.find_by(watchmode_id: id)
 
+    movie_pages = WatchmodeService.get_movies_by_service(id)
+
+    # ADD CONDITIONAL HERE - IF WATCHMODE API CALL SUCCESSFUL, DO THINGS, ELSE ERROR
     # MovieAvailabilities must be destroyed/recreated with each refresh
     # due to Watchmode API endpoint limitations & rate limits
-    MovieAvailability.where(service: service).destroy_all
+    ServiceUpdateJob.perform_later(service, movie_pages)
 
-    movie_pages = WatchmodeService.get_movies_by_service(id)
-    movie_pages.each do |movie_page|
-      movie_page[:titles].each do |movie_data|
-        movie = Movie.find_by(tmdb_id: movie_data[:tmdb_id])
-        unless movie
-          movie = Movie.create(
-            title: movie_data[:title],
-            tmdb_id: movie_data[:tmdb_id],
-            year: movie_data[:year]
-          )
-        end
-        movie.movie_availabilities.create(service: service)
-      end
-    end
     ServiceRefresh.new(service[:name], movie_pages[0][:total_results])
   end
 
