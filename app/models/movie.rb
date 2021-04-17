ca6363184c8b643b424009b6a5db6bab433048ef
Nick King <nickmaxking@gmail.com>
2021-04-17 13:38:25 -0600

    Add test and method for estimated_update movie class method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,20 @@ class Movie < ApplicationRecord
   has_many :services, through: :movie_availabilities
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+
+  def self.estimated_update_time(movie_count)
+    total_seconds = (movie_count.to_f / 100 * 23).round(0)
+    if total_seconds > 3599
+      hours = total_seconds / 3600
+      minutes = (total_seconds % 3600) / 60
+      seconds = total_seconds % 60
+      "#{hours} hour(s), #{minutes} minute(s), #{seconds} second(s)"
+    elsif total_seconds > 59
+      minutes = total_seconds / 60
+      seconds = total_seconds % 60
+      "#{minutes} minute(s), #{seconds} second(s)"
+    else
+      "#{total_seconds} second(s)"
+    end
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -14,4 +14,12 @@ describe Movie, type: :model do
       should have_many(:genres).through(:movie_genres)
     end
   end
+
+  describe 'class methods' do
+    it '.estimated_update_time' do
+      expect(Movie.estimated_update_time(44)).to eq('10 second(s)')
+      expect(Movie.estimated_update_time(261)).to eq('1 minute(s), 0 second(s)')
+      expect(Movie.estimated_update_time(16000)).to eq('1 hour(s), 1 minute(s), 20 second(s')
+    end
+  end
 end
