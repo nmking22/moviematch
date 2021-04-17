34652111c3298a10a327e07cae5ec33ff6e39c65
Nick King <nickmaxking@gmail.com>
2021-04-17 14:04:00 -0600

    Refactor estimated_update_time into helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,23 +6,43 @@ class Movie < ApplicationRecord
   has_many :movie_genres
   has_many :genres, through: :movie_genres
 
+  def self.needs_details
+    Movie.where(description:nil)
+  end
+  
   def self.estimated_update_time(movie_count)
     total_seconds = (movie_count.to_f / 100 * 23).round(0)
-    if total_seconds > 3599
-      hours = total_seconds / 3600
-      minutes = (total_seconds % 3600) / 60
-      seconds = total_seconds % 60
-      "#{hours} hour(s), #{minutes} minute(s), #{seconds} second(s)"
-    elsif total_seconds > 59
-      minutes = total_seconds / 60
-      seconds = total_seconds % 60
-      "#{minutes} minute(s), #{seconds} second(s)"
+    if hour_plus?(total_seconds)
+      time_in_hours(total_seconds)
+    elsif minute_plus?(total_seconds)
+      time_in_minutes(total_seconds)
     else
-      "#{total_seconds} second(s)"
+      time_in_seconds(total_seconds)
     end
   end
 
-  def self.needs_details
-    Movie.where(description:nil)
+  def self.hour_plus?(seconds)
+    seconds > 3599
+  end
+
+  def self.minute_plus?(seconds)
+    seconds > 59
+  end
+
+  def self.time_in_hours(total_seconds)
+    hours = total_seconds / 3600
+    minutes = (total_seconds % 3600) / 60
+    seconds = total_seconds % 60
+    "#{hours} hour(s), #{minutes} minute(s), #{seconds} second(s)"
+  end
+
+  def self.time_in_minutes(total_seconds)
+    minutes = total_seconds / 60
+    seconds = total_seconds % 60
+    "#{minutes} minute(s), #{seconds} second(s)"
+  end
+
+  def self.time_in_seconds(seconds)
+    "#{seconds} second(s)"
   end
 end
