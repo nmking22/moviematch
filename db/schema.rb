8bce521a55874d325820d2856851e1222ada2819
Nick King <nickmaxking@gmail.com>
2021-04-16 18:08:55 -0600

    Add tmdb_id column to genre table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_194034) do
+ActiveRecord::Schema.define(version: 2021_04_16_205955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_194034) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tmdb_id"
   end
 
   create_table "movie_availabilities", force: :cascade do |t|
