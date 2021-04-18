d66db9e977de8011e27643233412f4c9e83b4964
Nick King <nickmaxking@gmail.com>
2021-04-18 17:49:42 -0600

    Add helper methods to account for movies with no date or description info

--- app/poros/movie_info.rb
+++ app/poros/movie_info.rb
@@ -8,10 +8,26 @@ class MovieInfo
 
   def initialize(movie_data)
     @poster_path = movie_data[:poster_path]
-    @description = movie_data[:overview]
+    @description = add_description(movie_data)
     @vote_average = movie_data[:vote_average]
     @vote_count = movie_data[:vote_count]
-    @year = movie_data[:release_date].first(4)
+    @year = find_year(movie_data)
     @genres = movie_data[:genres]
+  end
+
+  def find_year(movie_data)
+    if movie_data[:release_date]
+      movie_data[:release_date].first(4)
+    else
+      'Unknown'
+    end
+  end
+
+  def add_description(movie_data)
+    if movie_data[:description]
+      movie_data[:description]
+    else
+      'No description available'
+    end
   end
 end
