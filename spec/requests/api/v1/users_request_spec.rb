c4150628d18c4d27898ae2fbb76b0a510d65f896
Nick King <nickmaxking@gmail.com>
2021-04-20 16:21:36 -0600

    Add test for users update action

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'users API' do
   it 'can create a user' do
     user_params = {
-      uid: '106220793108530540611',
+      uid: '12345678910',
       email: 'nickmaxking@gmail.com',
       first_name: 'Nick',
       last_name: 'King',
@@ -44,9 +44,9 @@ describe 'users API' do
     expect(json[:data][:attributes][:uid]).to eq(nick.uid)
   end
 
-  it 'can find an existing user' do
+  it 'can find an existing user with create action' do
     nick = User.create(
-      uid: '106220793108530540611',
+      uid: '12345678910',
       email: 'nickmaxking@gmail.com',
       first_name: 'Nick',
       last_name: 'King',
@@ -54,7 +54,7 @@ describe 'users API' do
     )
 
     user_params = {
-      uid: '106220793108530540611',
+      uid: '12345678910',
       email: 'nickmaxking@gmail.com',
       first_name: 'Nick',
       last_name: 'King',
@@ -91,6 +91,97 @@ describe 'users API' do
     expect(json[:data][:attributes][:email]).to eq(nick.email)
     expect(json[:data][:attributes]).to have_key(:image)
     expect(json[:data][:attributes][:image]).to eq(nick.image)
+    expect(json[:data][:attributes]).to have_key(:uid)
+    expect(json[:data][:attributes][:uid]).to eq(nick.uid)
+  end
+
+  it 'can find an existing user with show action' do
+    nick = User.create(
+      uid: '12345678910',
+      email: 'nickmaxking@gmail.com',
+      first_name: 'Nick',
+      last_name: 'King',
+      image: 'https://lh6.googleusercontent.com/-hEH5aK9fmMI/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucntLnugtaOVsqmvJGm89fFbDJ6GaQ/s96-c/photo.jpg'
+    )
+
+    get "/api/v1/users/#{nick.uid}"
+
+    json = JSON.parse(response.body, symbolize_names:true)
+
+    expect(response).to be_successful
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:data)
+    expect(json[:data]).to be_a(Hash)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:id]).to eq(nick.id.to_s)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data][:type]).to eq('user')
+    expect(json[:data]).to have_key(:attributes)
+    expect(json[:data][:attributes]).to be_a(Hash)
+    expect(json[:data][:attributes]).to have_key(:first_name)
+    expect(json[:data][:attributes][:first_name]).to eq(nick.first_name)
+    expect(json[:data][:attributes]).to have_key(:last_name)
+    expect(json[:data][:attributes][:last_name]).to eq(nick.last_name)
+    expect(json[:data][:attributes]).to have_key(:email)
+    expect(json[:data][:attributes][:email]).to eq(nick.email)
+    expect(json[:data][:attributes]).to have_key(:image)
+    expect(json[:data][:attributes][:image]).to eq(nick.image)
+    expect(json[:data][:attributes]).to have_key(:uid)
+    expect(json[:data][:attributes][:uid]).to eq(nick.uid)
+  end
+
+  it 'returns descriptive error for invalid show requests' do
+    get "/api/v1/users/12345678910"
+
+    json = JSON.parse(response.body, symbolize_names:true)
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(400)
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:error)
+    expect(json[:error]).to eq('User does not exist in database.')
+  end
+
+  it 'can update a user' do
+    nick = User.create(
+      uid: '12345678910',
+      email: 'nickmaxking@gmail.com',
+      first_name: 'Nick',
+      last_name: 'King',
+      image: 'https://lh6.googleusercontent.com/-hEH5aK9fmMI/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucntLnugtaOVsqmvJGm89fFbDJ6GaQ/s96-c/photo.jpg'
+    )
+
+    user_params = {
+      email: 'nickmaxking@example.com',
+      first_name: 'Trick',
+      last_name: 'Thing',
+      image: 'https://image.shutterstock.com/shutterstock/photos/1036253818/display_1500/stock-photo-image-of-excited-screaming-young-woman-standing-isolated-over-yellow-background-looking-camera-1036253818.jpg'
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/users/#{nick.uid}", headers: headers, params: JSON.generate(user_params)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:data)
+    expect(json[:data]).to be_a(Hash)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:id]).to eq(nick.id.to_s)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data][:type]).to eq('user')
+    expect(json[:data]).to have_key(:attributes)
+    expect(json[:data][:attributes]).to be_a(Hash)
+    expect(json[:data][:attributes]).to have_key(:first_name)
+    expect(json[:data][:attributes][:first_name]).to eq(user_params[:first_name])
+    expect(json[:data][:attributes]).to have_key(:last_name)
+    expect(json[:data][:attributes][:last_name]).to eq(user_params[:last_name])
+    expect(json[:data][:attributes]).to have_key(:email)
+    expect(json[:data][:attributes][:email]).to eq(user_params[:email])
+    expect(json[:data][:attributes]).to have_key(:image)
+    expect(json[:data][:attributes][:image]).to eq(user_params[:image])
     expect(json[:data][:attributes]).to have_key(:uid)
     expect(json[:data][:attributes][:uid]).to eq(nick.uid)
   end
