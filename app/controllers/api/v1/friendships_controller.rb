3f73cf27fc86fcc84eded27b171b763385636ac8
Nick King <nickmaxking@gmail.com>
2021-05-05 02:24:44 -0600

    Add conditional for invalid email sadpath on create action

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::FriendshipsController < ApplicationController
   def create
-    friendship = Friendship.create(friendship_params)
-    render json: FriendshipSerializer.new(friendship)
+    friend = User.find_by(email: friendship_params[:friend_email])
+    if friend
+      friendship = Friendship.create(
+        user_id: friendship_params[:user_id],
+        friend: friend
+      )
+      render json: FriendshipSerializer.new(friendship)
+    else
+      output = {
+        error: 'Invalid email. Friend not added.'
+      }
+      render json: output, :status => 400
+    end
   end
 
   private
 
   def friendship_params
-    params.permit(:user_id, :friend_id)
+    params.permit(:user_id, :friend_email)
   end
 end
