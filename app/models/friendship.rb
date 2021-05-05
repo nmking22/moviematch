80d78b5c9c377f2fce5ffd8ea10a613fcef2cb33
Nick King <nickmaxking@gmail.com>
2021-05-05 16:31:34 -0600

    Add test and functionality for friendship exists? class method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,8 @@ class Friendship < ApplicationRecord
                         :friend_id
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def self.exists?(user_id, friend_id)
+    Friendship.where(user_id: user_id).where(friend_id: friend_id) != []
+  end
 end
